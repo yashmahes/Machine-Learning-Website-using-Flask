8cb82806a97cecea5ca4ef26a5772b4394ed016d
Sakshi Lakshkar <sakshi.lakshkar@ranosys.com>
2018-10-26 10:22:14 +0530

    mysite python apis

---
+++ mysite.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+app.run(debug=True)
