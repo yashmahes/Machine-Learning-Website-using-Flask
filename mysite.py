2267a39d15c8306617dfbf59a2da866c57c49061
yash maheshwari <yashmahes@gmail.com>
2018-10-29 08:59:28 +0530

    connecting to sqlalchemy database

--- mysite.py
+++ mysite.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+import json
+from flask_sqlalchemy import SQLAlchemy 
+
+
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -12,9 +19,21 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/contact")
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        ''' Add entry to the database '''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name, email, phone, message, datetime.now())
+
+
+
     return render_template('contact.html')
+
 
 @app.route("/post")
 def post():
